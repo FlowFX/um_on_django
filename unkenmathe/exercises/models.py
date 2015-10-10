cf42904854b486d951579e294db40ab7c0975a71
Florian Posdziech <hallo@flowfx.de>
2015-10-10 13:54:32 -0500

    Add first model

--- unkenmathe/exercises/models.py
+++ unkenmathe/exercises/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Exercise(models.Model):
+	id = models.AutoField(primary_key=True)
+	slug = models.PositiveIntegerField(unique=True)
+	content = models.TextField()
