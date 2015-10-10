a550977d6efe601c033a8023d21da968e01b2319
Florian Posdziech <hallo@flowfx.de>
2015-10-10 14:56:24 -0500

    Introduce TimeStampedModel

--- unkenmathe/core/models.py
+++ unkenmathe/core/models.py
@@ -1,3 +1,16 @@
+# core/models.py
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+	"""
+	An abstract base class model that provides self-
+	updating ``created`` and ``modified`` fields.
+
+	cf. Two Scoops of Django 1.8 p.66
+	"""
+	created = models.DateTimeField(auto_now_add=True)
+	modified = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		abstract = True
--- unkenmathe/exercises/models.py
+++ unkenmathe/exercises/models.py
@@ -1,7 +1,10 @@
+# exercises/models.py
 from django.db import models
 
+from unkenmathe.core.models import TimeStampedModel
+
 # Create your models here.
-class Exercise(models.Model):
+class Exercise(TimeStampedModel):
 	id = models.AutoField(primary_key=True)
 	slug = models.PositiveIntegerField(unique=True)
 	content = models.TextField()
