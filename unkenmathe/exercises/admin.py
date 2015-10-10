c6cd813a5f7cf4fd1167b03f85997f41274ee83c
Florian Posdziech <hallo@flowfx.de>
2015-10-10 14:56:40 -0500

    Add model "solutions"

--- unkenmathe/exercises/admin.py
+++ unkenmathe/exercises/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
 # import your models here.
-from unkenmathe.exercises.models import Exercise
+from unkenmathe.exercises.models import Exercise,Solution
 
 class ExerciseAdmin(admin.ModelAdmin):
 	model = Exercise
-	list_display = ('id', 'slug', 'content',)
+	list_display = ('id', 'slug', 'content', 'created',)
+
+class SolutionAdmin(admin.ModelAdmin):
+	model = Solution
+	list_display = ('exercise', 'content',)
+	readonly_fields = ('exercise',)
+
 
 # and register them.
 admin.site.register(Exercise, ExerciseAdmin)
+admin.site.register(Solution, SolutionAdmin)
--- unkenmathe/exercises/models.py
+++ unkenmathe/exercises/models.py
@@ -8,3 +8,13 @@ class Exercise(TimeStampedModel):
 	id = models.AutoField(primary_key=True)
 	slug = models.PositiveIntegerField(unique=True)
 	content = models.TextField()
+
+	def __str__(self):
+		return str(self.slug)
+
+class Solution(TimeStampedModel):
+	exercise = models.OneToOneField(Exercise, primary_key=True)
+	content = models.TextField()
+
+	def __str__(self):
+		return str(self.exercise.slug)
