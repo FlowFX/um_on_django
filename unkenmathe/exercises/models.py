83eedde6ec94b4bca2081b8efcdb0b8708f1e8e0
Florian Posdziech <hallo@flowfx.de>
2015-10-10 14:58:41 -0500

    Add documentation to exercises classes

--- unkenmathe/exercises/models.py
+++ unkenmathe/exercises/models.py
@@ -5,6 +5,9 @@ from unkenmathe.core.models import TimeStampedModel
 
 # Create your models here.
 class Exercise(TimeStampedModel):
+	"""
+	The class model for exercises.
+	"""
 	id = models.AutoField(primary_key=True)
 	slug = models.PositiveIntegerField(unique=True)
 	content = models.TextField()
@@ -13,6 +16,10 @@ class Exercise(TimeStampedModel):
 		return str(self.slug)
 
 class Solution(TimeStampedModel):
+	"""
+	The class model for solutions.
+	Each solution corresponds to exactly one exercise.
+	"""
 	exercise = models.OneToOneField(Exercise, primary_key=True)
 	content = models.TextField()
 
